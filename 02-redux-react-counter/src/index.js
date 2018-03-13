a6e01b14912c954d8443a0ac3e5c2b73259476d3
Aliaksei Huk <alexeyguk@gmail.com>
2018-03-13 21:41:21 +0300

    move store to separate file

--- 02-redux-react-counter/src/index.js
+++ 02-redux-react-counter/src/index.js
@@ -1,53 +1,11 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore } from 'redux';
+import {store} from './store/createStore';
 
-const reducer = (state = 0, action) => {
-  switch(action.type) {
-    case 'INCREMENT':
-      return state + 1;
-    case 'DECREMENT':
-      return state - 1;
-    default:
-      return state;
-  }
-}
 
-const store = createStore(reducer);
 
-const Counter = (props) => {
-  return (
-    <section>
-      {props.value}
-      <button onClick={props.onIncrement}>+</button>
-      <button onClick={props.onDecrement}>-</button>
-    </section>
-  );
-}
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    store.subscribe(() => {
-      this.setState({value: store.getState()});
-    });
-
-    this.state = {
-      value: store.getState()
-    }
-  }
-
-  render() {
-    return (
-      <Counter
-        value={this.state.value}
-        onIncrement={() => store.dispatch({type: 'INCREMENT'})}
-        onDecrement={() => store.dispatch({type: 'DECREMENT'})}/>
-    )
-  }
-}
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App store={store}/>, document.getElementById('root'));
 registerServiceWorker();
