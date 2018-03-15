2c532f2f4e677d6cfe9cca864410d1daf9344b62
Aliaksei Huk <alexeyguk@gmail.com>
2018-03-15 19:58:20 +0300

    add finished

---
+++ 08-todolist/src/components/TodoList.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import Todo from './Todo';
+
+class TodoList extends Component {
+  constructor(props) {
+    super(props);
+
+    props.store.subscribe(() => this.forceUpdate());
+  }
+
+  render() {
+    
+    const list = this.props.store.getState().todos
+      .filter(todo => todo.text.includes(this.state.filter))
+      .map(todo =>
+        <Todo
+          store={this.props.store} 
+          key={todo.id}
+          todo={todo}
+        />
+      );
+    return (
+      <section className="TodoList">
+        {list}
+      </section>
+    )
+  }
+}
+
+export default TodoList;
